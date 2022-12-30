e21c4c4d43c7e819e81d6826c7fa0740d74bc336
Maria Pasaylo <90634830+mariapasaylo@users.noreply.github.com>
2022-12-29 22:26:35 -0500

    mobile menu works
    Animated Hamburger menu for mobile. 
    Temporarily removed hero because the text bleeds over in mobile version.

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,30 @@
-//constants
-const btnHambrgr = document.querySelector('btnHmbrgr');
+//Constants
+const btnHambrgr = document.querySelector('#btnHambrgr');
 const header = document.querySelector('.header');
-const overlay = document.querySelector('.overlay')
-const fadeElems = document.querySelectorAll('.has-fade');
-const body = document.querySelector('body');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.fade');
 
 btnHambrgr.addEventListener('click', function(){
 
-    
+    if(header.classList.contains('open')){
+        console.log('close hamburger');
+        header.classList.remove('open'); //animates burger to close on click
+
+      //fade-out the overlay and mobile menu
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
+    }
+    else{
+        console.log('open hamburger');
+        header.classList.add('open'); //animates burger to open on click
+
+        //fade-in the overlay and mobile menu
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
+    }
+
 });
